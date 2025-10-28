bb52f8f2745f4f3f0282a315d8e39c9b72bea044
Bassam Alghamdi <bassam.ghk@gmail.com>
2025-10-28 10:56:05 +0300

    Add Product and Category serializers

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
+from .models import Category, Product, Order, OrderItem
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
@@ -24,3 +25,33 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email')
+
+class CategorySerializer(serializers.ModelSerializer):
+    products_count = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'description', 'products_count', 'created_at']
+    
+    def get_products_count(self, obj):
+        return obj.products.filter(is_active=True).count()
+
+class ProductSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name', read_only=True)
+    user_username = serializers.CharField(source='user.username', read_only=True)
+    is_in_stock = serializers.BooleanField(read_only=True)
+    is_low_stock = serializers.BooleanField(read_only=True)
+    
+    class Meta:
+        model = Product
+        fields = [
+            'id', 'user', 'user_username', 'category', 'category_name',
+            'name', 'description', 'price', 'stock_quantity', 'sku',
+            'image_url', 'is_active', 'is_in_stock', 'is_low_stock',
+            'created_at', 'updated_at'
+        ]
+        read_only_fields = ['user', 'created_at', 'updated_at']
+    
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
