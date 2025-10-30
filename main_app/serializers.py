33982fc41c21f6f639d7a0ed99f799c25aeac16a
Bassam Alghamdi <bassam.ghk@gmail.com>
2025-10-30 17:37:08 +0300

    fix: include is_staff in user profile response to show admin link

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -24,7 +24,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'username', 'email', 'is_staff')
 
 class CategorySerializer(serializers.ModelSerializer):
     products_count = serializers.SerializerMethodField()
