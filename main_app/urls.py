3c0e843ace58b98e42301972d3337a58128e1949
Bassam Alghamdi <bassam.ghk@gmail.com>
2025-10-30 11:06:07 +0300

    Test order endpoints - all working

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, ProfileView, CategoryViewSet, ProductViewSet
+from .views import RegisterView, ProfileView, CategoryViewSet, ProductViewSet, OrderViewSet
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'products', ProductViewSet, basename='product')
+router.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='register'),
